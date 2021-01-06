3d43758bbb9f40a95e1cecab1acc660817a173b0
Nikolay <70325611+f0kycNIK@users.noreply.github.com>
2021-01-06 22:21:08 +0300

    исправил скачиние с Хабл

--- main.py
+++ main.py
@@ -18,10 +18,8 @@ def fetch_spacex_last_launch(url):
     payload = {}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
-    # pprint(response.text.encode('utf8'))
     fly_lib = response.json()
     url_images = fly_lib[13]['links']['flickr']['original']
-    #pprint(url_images)
     return url_images
 
 def fetch_photo_hubble (url):
@@ -29,12 +27,15 @@ def fetch_photo_hubble (url):
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
     response.raise_for_status()
-    # pprint(response.json())
     fly_lib = response.json()
-    name =  fly_lib[0]['id']
-    # print (name)
-    collection_url = f' http://hubblesite.org/api/v3/image/{name}'
-    # print(collection_url)
+    image_id =  fly_lib[0]['id']
+    return image_id
+
+
+def creating_list_url_image (image_id):
+    payload = {}
+    headers = {}
+    collection_url = f' http://hubblesite.org/api/v3/image/{image_id}'
     response = requests.request("GET", collection_url, headers=headers, data=payload)
     response.raise_for_status()
     pprint(response.json())
@@ -43,24 +44,17 @@ def fetch_photo_hubble (url):
     lst = []
     for image in images:
         lst.append(image['file_url'])
-    image_url1 = fly_lib2['image_files'][0]['file_url']
-    # image_url2 = fly_lib2['image_files'][1]['file_url']
-    # print(image_url1, '\n', image_url2)
     print(lst)
-    return lst
+    return(lst)
 
-def save_picture_any_format (urls):
-    for url in urls:
-        payload = {}
-        headers = {}
-        print(url)
-        lst = url.split('.')
+def save_picture_any_format (list_images_url, image_id):
+    payload = {}
+    headers = {}
+    for image_url in list_images_url:
+        lst = image_url.split('.')
         image_format = lst[-1]
-        filename = f'hubble_images/hubble.{image_format}'
-        print(filename)
-        mm = 'https:' + url
-        print(mm)
-        response = requests.get(mm, headers=headers, data=payload)
+        filename = f'hubble_images/{image_id}.{image_format}'
+        response = requests.get('https:' + image_url, headers=headers, data=payload, verify=False)
         response.raise_for_status()
         with open(filename, 'wb') as file:
             file.write(response.content)
@@ -72,9 +66,10 @@ if __name__ == '__main__':
     hubble_url = 'https://hubblesite.org/api/v3/images/news'
     url_images = fetch_spacex_last_launch(spacex_url)
     download_imges(url_images)
-    image_hubble_url = fetch_photo_hubble(hubble_url)
+    image_id = fetch_photo_hubble(hubble_url)
     Path("hubble_images").mkdir(parents=True, exist_ok=True)
-    save_picture_any_format(image_hubble_url)
+    lst = creating_list_url_image(image_id)
+    save_picture_any_format(lst, image_id)
 
 
 
